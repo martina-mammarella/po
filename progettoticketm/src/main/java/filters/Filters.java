d0bd0aa2da6dff3c9656ee52c6193592d974c981
marty <marty@LAPTOP-G5ITQDIQ.homenet.telecomitalia.it>
2021-02-11 13:45:52 +0100

    filtrin1

--- progettoticketm/src/main/java/filters/Filters.java
+++ progettoticketm/src/main/java/filters/Filters.java
@@ -1,64 +1,37 @@
 package filters;
-import java.util.Vector;
-import java.util.List;
-import org.json.simple.JSONObject;
-import evento.modello.*;
-import exception.NotFoundException;
-import org.json.simple.*;
-
-public class Filters {
-	protected String valoreFiltro;
-	protected List<Filters>f= new Vector<>();
-	protected List<Filters>fCountry= new Vector<>();
-	protected List<Filters>fType= new Vector<>();
-	public Filters() {};
-;	public Filters(String valoreFiltro) {
-		setValoreFiltro(valoreFiltro);
-	}
-	protected void setValoreFiltro(String valoreFiltro) {
-		this.valoreFiltro=valoreFiltro;
-	}
-	protected String getValoreFiltro() {
-		return valoreFiltro;
-	}
-	
-	public void parseFilteres(JSONObject filtroFornito) {
-		if(filtroFornito.containsKey("countryCode")) {
-			String countryCode=(String)filtroFornito.get("countryCode");
-			Filters x= new Filters();
-		for(String c:x.parseString(countryCode)) {
-			Filters xCountry=new FiltersCountry(c);
-			fCountry.add(xCountry);
-		}
-			
-		}
-		if(filtroFornito.containsKey("type")) {
-			Filters x= new Filters();
-			String type=(String)filtroFornito.get("type");
-			for(String t:x.parseString(type)) {
-				Filters x1=new FiltersType(t);
-				fType.add(x1);
+	import java.util.Vector;
+	import java.util.List;
+	import org.json.simple.JSONObject;
+	import evento.modello.*;
+	import exception.NotFoundException;
+	import org.json.simple.*;
+    import org.json.simple.JSONArray;
+    import java.lang.*;
+public  class Filters {
+	protected List<Filters>filtri=new Vector();
+	protected List<Filters>fCountry= new Vector();
+	protected List<Filters>fType=new Vector();
+	public void tipoFiltro(JSONObject filtro) {
+		try {
+			if(filtro.containsKey("countryCode")) {
+			FiltersCountry x= new FiltersCountry((String)filtro.get("countryCode"));
+			fCountry.add(x);
+				
 			}
-			
+			if(filtro.containsKey("type")) {
+			FiltersType x= new FiltersType((String)filtro.get("type"));
+			fType.add(x);
+			}
+		}catch(Exception e) {
+			System.out.println("non sono stati applicati filtri");
 		}
-		
-		}
-	public String[] parseString(String line){
-		String[] s=null;
-	    s=line.split(";");
-		return s;
 	}
-	public List<Filters> getFilters(){
-		return f;
-	}
-	public  void filtraggio(List<Eventi> dafiltrare, List<Eventi>filtrato ) {};
+	public void filtraggio(List<Eventi> dafiltrare,List <Eventi>filtrati) {};
 	
-	public List<Filters>  getFiltersCountry() {
-		return fCountry;
-	}
-	public List<Filters>  getFiltersType() {
-		return fType;
-	}
 	
-
+	
+	
+	
+	
+	
 }
